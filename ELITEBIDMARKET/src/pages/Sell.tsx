9ac569a1c460240618f807534d8b6a1f07ecf997
LenovoX13 <damir.tliche.499@my.csun.edu>
2023-11-11 09:18:18 -0800

    Added image logo to the "Sell" webpage and fixed the cosmetics of the webpage with displaying the text in fashionable order.

--- ELITEBIDMARKET/src/pages/Sell.tsx
+++ ELITEBIDMARKET/src/pages/Sell.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { InputText, MessageInput, Button } from "../components";
-
+import {post_auction_image} from "../assets";
 const Sell = () => {
+  // @ts-ignore
   return (
     <div className="bg-primary flex justify-around p-10 text-subtext">
       {/* Left Section */}
       <div className="flex flex-col w-full h-full">
         <div className="font-bold w-[660px]">
-          Turn Your Items into Opportunities{" "}
+          <span className="text-black">Turn Your Items into Opportunities</span>{" "}
           <span className="text-white">List Your Auction Today!</span>
         </div>
         <div className="flex w-full gap-5">
@@ -35,8 +36,14 @@ const Sell = () => {
       </div>
 
       {/* right Section */}
-      <div className="flex w-full h-full">Post an action</div>
-    </div>
+      <div className="font-bold flex w-full h-full text-center flex-col text-subtitle">Post an Auction
+        <center><img src={post_auction_image} className="App-logo w-image object-center" alt="logo"/></center>
+        <div className="w-[660px] font-semibold text-right rtl:text-left">
+          <span className="text-black">Unleash Your Items' Potential</span><br></br>
+            <span className="text-white">Begin Your Auction Journey!</span>
+        </div>
+      </div>
+      </div>
   );
 };
 
