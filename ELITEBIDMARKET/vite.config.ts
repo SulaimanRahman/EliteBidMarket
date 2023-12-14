9dbe87a6ad7ffa4e2b94e1fa5eee40ccb49b280d
koayenay <arfan.myint.607@my.csun.edu>
2023-12-14 12:39:04 -0800

    config

--- ELITEBIDMARKET/vite.config.ts
+++ ELITEBIDMARKET/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "tailwindcss"
+import autoprefixer from "autoprefixer"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Additional configurations
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+
+  // MUI Library configuration
+  optimizeDeps: {
+    include: ["@mui/material", "@emotion/react", "@emotion/styled"],
+  },
+
+  // TypeScript configuration
+  // Make sure you have "tsconfig.json" in your project
+  // Vite automatically detects it and uses TypeScript.
 })
